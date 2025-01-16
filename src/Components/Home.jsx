800bf2dc799f8477b91b6d1f6d0137b9b772b583
swetha <swetha@swethas-MacBook-Air.local>
2025-01-16 18:55:49 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,14 @@
-// Home.js
-import React, { useEffect, useState } from 'react';
-import './Home.css'; // Import the CSS file
+import React, { useEffect, useState, useMemo } from "react";
+import "./Home.css"; // Import the CSS file
 
-const Home = ({ isDarkMode }) => { // Receive dark mode state as a prop
-  const [title, setTitle] = useState('');
-  const titles = ["Full Stack Developer", "Web Developer", "MERN Stack Developer"];
+const Home = ({ isDarkMode }) => {
+  const [title, setTitle] = useState("");
   const [titleIndex, setTitleIndex] = useState(0);
   const [charIndex, setCharIndex] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
+
+  // Memoize the titles array to prevent it from being redefined on each render
+  const titles = useMemo(() => ["Full Stack Developer", "Web Developer", "MERN Stack Developer"], []);
 
   useEffect(() => {
     const handleTyping = () => {
@@ -37,22 +38,34 @@ const Home = ({ isDarkMode }) => { // Receive dark mode state as a prop
   }, [charIndex, titleIndex, isDeleting, titles]);
 
   return (
-    <div className={`home ${isDarkMode ? 'dark-theme' : ''}`} id="home"> 
+    <div className={`home ${isDarkMode ? "dark-theme" : ""}`} id="home">
       <div className="text-container">
         <h1>Hello,</h1>
-        <h2>It's <span className="highlight bouncing-effect">Swetha</span></h2>
-        <h3>And I'm into</h3> 
-        <h3><span className="highlight">{title}</span></h3>
+        <h2>
+          It's <span className="highlight bouncing-effect">Swetha</span>
+        </h2>
+        <h3>And I'm into</h3>
+        <h3>
+          <span className="highlight">{title}</span>
+        </h3>
         <p>
-          Passionate about delivering innovative and efficient engineering solutions that drive meaningful impact and foster progress. Continuously seeking opportunities to apply my skills, collaborate with diverse teams, and contribute to forward-thinking projects that inspire growth and innovation.
+          Passionate about delivering innovative and efficient engineering
+          solutions that drive meaningful impact and foster progress.
+          Continuously seeking opportunities to apply my skills, collaborate
+          with diverse teams, and contribute to forward-thinking projects that
+          inspire growth and innovation.
         </p>
         <button className="about-button">More About Me</button>
       </div>
       <div className="image-container">
-        <img src="/images/PHOTO-2025-01-13-15-53-14.jpg" alt="Profile" className="profile-image" />
+        <img
+          src="/images/PHOTO-2025-01-13-15-53-14.jpg"
+          alt="Profile"
+          className="profile-image"
+        />
       </div>
     </div>
   );
-};  
+};
 
 export default Home;
